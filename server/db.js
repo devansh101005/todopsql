334f0816211e88fc19aa45b3683bdab06c30a668
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-24 10:48:14 +0530

    yyy

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
 const Pool =require('pg').Pool;
 
 const pool = new Pool({
-    user:"Postgres",
+    user:"postgres",
     password:"Devansh123",
     host:"localhost",
     port:5432,
     database:"perntodo"
 });
+
+module.exports=pool;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,61 @@
 const express= require('express');
 const app=express();
 const cors =require("cors");
+const pool=require("./db");
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+//create a todo
+app.post("/todos",async(req,res) => {
+    try {
+       // console.log(req.body);
+       const {description}=req.body;
+       const newTodo = await pool.query(
+        "INSERT INTO todo (description) VALUES($1) RETURNING * ",
+        [description]
+       );
+       res.json(newTodo);
+
+    }
+        catch (err) {
+            console.error(err.message);
+        }
+})
+
+//get all todo
+
+app.get("/todos", async(req,res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+    
+})
+
+//get a todo
+app.get("/todos/:id",async (req,res) => {
+    try {
+       // console.log(req.params) 
+         const {id} = req.params;
+            const todo = await pool.query
+            ("SELECT * FROM todo WHERE todo_id = $1", [id]);
+            res.json(todo.rows[0]);
+    }
+    catch (err) {
+        console.error(err.message);
+    }
+})
+
+
+//updare a todo
+
+//delete a toso
 
 
 app.listen(5000,() => {
