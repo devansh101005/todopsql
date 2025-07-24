4f05d99bedfc2e038d247bb0505b955e9b821481
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-24 22:07:58 +0530

    yyy

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import React ,{Fragment} from 'react';
+import './App.css';
+
+//components
+import inputTodo from "./components/inputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <inputTodo />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/inputTodo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const inputTodo =() => {
+    return <h1> Input todo</h1>;
+};
+export default inputTodo;
