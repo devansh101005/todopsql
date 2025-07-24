050d5ae0d90a5e35c7563026c15915314567c62d
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-24 21:43:11 +0530

    yyy

--- server/index.js
+++ server/index.js
@@ -54,8 +54,34 @@ app.get("/todos/:id",async (req,res) => {
 
 
 //updare a todo
+app.put("/todos/:id", async (req,res) => {
+    try {
+        const {id} =req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description =$1  WHERE todo_id =$2",
+            [description, id]
+        );
+        res.json("Todo was updated!");
+
+    }
+    catch(err) {
+        console.error(err.message);
+    }
+
+
+})
 
 //delete a toso
+app.delete("/todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        res.json("Todo was deleted!");
+    }
+    catch(err) {
+        console.error(err.message);
+    }
+})
 
 
 app.listen(5000,() => {
